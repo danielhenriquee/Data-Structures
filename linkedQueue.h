ef83d97d7ed5e769031bea50f13ad0aa514caab8
Daniel Henrique <138157786+danielhenriquee@users.noreply.github.com>
2025-08-12 15:36:22 -0300

    Update linkedQueue.h

--- linkedQueue.h
+++ linkedQueue.h
@@ -1,30 +1,32 @@
 #ifndef LINKED_QUEUE_H
 #define LINKED_QUEUE_H
 
+#include <stdexcept>
+
 // Generic element
 template <typename T>
-struct TElementQ {
+struct LinkedQ_TElement {
     T data;
-    TElementQ<T> *next; // Points to next element
+    LinkedQ_TElement<T> *next; // Points to next element
 };
 
 // Points to the first element of the Generic Linked Queue
 template <typename T>
-struct TListQ {
-    TElementQ<T> *first;
+struct LinkedQ_TList {
+    LinkedQ_TElement<T> *first;
 };
 
 // Boot Generic Linked Queue
 template <typename T>
-bool bootLinkedQueue(TListQ<T> &list) {
+bool LinkedQ_boot(LinkedQ_TList<T> &list) {
     list.first = nullptr;
     return true;
 }
 
 // Insert at the end of the queue
 template <typename T>
-bool queueL(TListQ<T> &list, T &data) {
-    TElementQ<T> *e = new TElementQ<T>;
+bool LinkedQ_enqueue(LinkedQ_TList<T> &list, const T &data) {
+    LinkedQ_TElement<T> *e = new LinkedQ_TElement<T>;
     e->data = data;
     e->next = nullptr;
   
@@ -33,7 +35,7 @@ bool queueL(TListQ<T> &list, T &data) {
         return true;
     }
       
-    TElementQ<T> *nav = list.first; // Create auxiliary pointer to navigate the queue
+    LinkedQ_TElement<T> *nav = list.first; // Create auxiliary pointer to navigate the queue
     while (nav->next != nullptr)
         nav = nav->next; // Navigate to the last element in the queue
     nav->next = e; // Insert element at the end of the queue
@@ -42,17 +44,19 @@ bool queueL(TListQ<T> &list, T &data) {
 
 // Remove first element in the queue
 template <typename T>
-T dequeueL(TListQ<T> &list) {
-  if (list.first == nullptr) // Check if queue is empty
-      return false;
-  
-  T data = list.first->data; // Save data of the first element
-  list.first = list.first->next; // list.first points to second element
-  return data; // Return data from first element
+T LinkedQ_dequeue(LinkedQ_TList<T> &list) {
+    if (list.first == nullptr) // Check if queue is empty
+        throw std::underflow_error("Error: empty queue.");
+
+    LinkedQ_TElement<T> *temp = list.first;
+    T data = list.first->data; // Save data of the first element
+    list.first = list.first->next; // list.first points to second element
+    delete temp;
+    return data; // Return data from first element
 }
 
 template <typename T>
-bool isEmptyL(const TListQ<T> &list) {
+bool LinkedQ_isEmpty(const LinkedQ_TList<T> &list) {
     return list.first == nullptr;
 }
 
