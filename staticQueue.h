27578b3479d9d6b66af1f4438f29ee0e49047f3c
Daniel Henrique <138157786+danielhenriquee@users.noreply.github.com>
2025-08-12 15:38:28 -0300

    Update staticQueue.h

--- staticQueue.h
+++ staticQueue.h
@@ -1,49 +1,51 @@
 #ifndef STATIC_QUEUE_H
 #define STATIC_QUEUE_H
 
+#include <stdexcept>
+
 // Generic element
 template <typename T>
-struct TElementQ {
+struct StaticQ_TElement {
     T data;
 };
 
 // Generic Static Queue
 template <typename T, int MAX>
-struct TListQ {
-    TElementQ<T> elements[MAX];
+struct StaticQ_TList {
+    StaticQ_TElement<T> elements[MAX];
     int size; // Counter of elements' number
 };
 
 // Boot Generic Static Queue
 template <typename T, int MAX>
-void bootStaticQueue(TListQ<T, MAX> &list) {
+void StaticQ_boot(StaticQ_TList<T, MAX> &list) {
     list.size = 0;
 }
 
 // Insert at the end of the queue
 template <typename T, int MAX>
-bool queueS(TListQ<T, MAX> &list, T data) {
-  if (list.size >= MAX) // Check if list is full
-      return false;
-
-  TElementQ<T> e;
-  e.data = data;
-  list.elements[list.size] = e; // Insert element at last position
-  list.size++; // Increase queue size
-  return true;
+bool StaticQ_enqueue(StaticQ_TList<T, MAX> &list, T data) {
+    if (list.size >= MAX) // Check if list is full
+        throw std::underflow_error("Error: empty queue.");
+    
+    StaticQ_TElement<T> e;
+    e.data = data;
+    list.elements[list.size] = e; // Insert element at last position
+    list.size++; // Increase queue size
+    return true;
 }
 
 // Remove first element in the queue
 template <typename T, int MAX>
-T dequeueS(TListQ<T, MAX> &list) {
-  if (list.size == 0) // Check if queue is empty
-      return false;
+T StaticQ_dequeue(StaticQ_TList<T, MAX> &list) {
+    if (list.size == 0) // Check if queue is empty
+        return false;
   
-  T data = list.elements[0].data; // Save data of the first element
-  for (int i = 0; i < list.size - 1; i++)
-      list.elements[i] = list.elements[i + 1];
-  list.size--;
-  return data; // Return data from first element
+    T data = list.elements[0].data; // Save data of the first element
+    for (int i = 0; i < list.size - 1; i++)
+        list.elements[i] = list.elements[i + 1];
+    list.size--;
+    return data; // Return data from first element
 }
 
 #endif // STATIC_QUEUE_H
